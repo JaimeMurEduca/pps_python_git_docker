a6c7a7345eecbf9248fd295e7ebc7f8653eb5c7a
jaime <4206180@alu.murciaeduca.es>
2024-02-05 18:37:10 +0100

    Experto en WEB

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-print('Hola mundo')
+from flask import Flask, jsonify
+from bayeta import frotar
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>!Hola mundo!</p>"
+
+@app.route('/frotar/<int:n_frases>', methods=['GET'])
+def frases(n_frases):
+    frases_generadas = frotar(n_frases)
+    return jsonify({"frases": frases_generadas})
+
+app.run(host="0.0.0.0")
---
+++ bayeta.py
@@ -0,0 +1,9 @@
+def frotar(n_frases: int = 1) -> list():
+    lista = list()
+    
+    for frase in range(n_frases):
+        lista.append('Hola mundo!') 
+
+    return lista
+
+print(frotar(5))
